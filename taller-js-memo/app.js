4a49f7f1bec2bd95f4572c3009aa0486b1d67184
Herdez <mherdez@users.noreply.github.com>
2023-05-18 14:03:05 -0600

    confetti

--- taller-js-memo/app.js
+++ taller-js-memo/app.js
@@ -1,43 +1,70 @@
+
 // Seleccionamos el contenedor del DOM
 const container = document.querySelector('.container');
 
 // Revolvemos el arreglo del memorama
-const suffle = ( arr ) => arr.sort( () => Math.random() - 0.5)
+const suffle = (arr) => arr.sort(() => Math.random() - 0.5);
 
 // Figuras del memorama
-const figuras = ['🐶', '🐱', '🦁', '🐼', '🐮', '🐰']
+const figuras = ['🐶', '🐱', '🦁', '🐼', '🐮', '🐰'];
 
 // Creamos el arreglo a traves de la funcion suffle
 const animales = suffle([...figuras, ...figuras]);
 
 // Visualizar las tarjetas del memorama
-animales.forEach( (animal, index) => container.innerHTML +=  `<div class="box" onclick="handleClick(${index})">?</div>`)
+animales.forEach((animal, index) => container.innerHTML += `<div class="box" onclick="handleClick(${index})">?</div>`);
 
-let parejas = []
-let parejasEncontradas = []
+let parejas = [];
+let parejasEncontradas = [];
 
 // Funcion handleClick()
 const handleClick = (id) => {
 
-  const boxes = document.querySelectorAll('.box')
+  const boxes = document.querySelectorAll('.box');
 
-  boxes[id].textContent = animales[id]
-  parejas.push(id)
-  if( parejas.length == 2 ) {
+  boxes[id].textContent = animales[id];
+  parejas.push(id);
+  if (parejas.length == 2) {
     if (animales[parejas[0]] === animales[parejas[1]]) {
-      parejasEncontradas.push(animales[parejas[0]])
-      console.log(parejasEncontradas)
-      parejas = []
-      if( parejasEncontradas.length === 6) {
-        console.log('juego concluido')
+      parejasEncontradas.push(animales[parejas[0]]);
+      console.log(parejasEncontradas);
+      parejas = [];
+      if (parejasEncontradas.length === 6) {
+        var end = Date.now() + (5 * 1000);
+        // go Buckeyes!
+        var colors = ['#000', '#ff0000', '#f3f3f3'];
+        (function frame() {
+          confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: {
+              x: 0
+            },
+            colors: colors
+          });
+          confetti({
+            particleCount: 3,
+            angle: 120,
+            spread: 55,
+            origin: {
+              x: 1
+            },
+            colors: colors
+          });
+          if (Date.now() < end) {
+            requestAnimationFrame(frame);
+          }
+        }());
       }
     } else {
-      setTimeout( () => {
-        boxes[parejas[0]].textContent = '?'
-        boxes[parejas[1]].textContent = '?'
-        parejas = []
-      },500)
+      setTimeout(() => {
+        boxes[parejas[0]].textContent = '?';
+        boxes[parejas[1]].textContent = '?';
+        parejas = [];
+      }, 500);
     }
   }
 }
+
 
